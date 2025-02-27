5ade39b842ebe49259c2f503c0713e941e29ea70
Josh McDonald <jmcdonald@innovarhealthcare.com>
2025-02-27 16:37:45 -0600

    debugging jar downloads for extensions

--- src/main/java/com/innovarhealthcare/launcher/MirthJNLPLauncher.java
+++ src/main/java/com/innovarhealthcare/launcher/MirthJNLPLauncher.java
@@ -14,15 +14,15 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.rmi.server.LogStream.log;
 
 public class MirthJNLPLauncher extends Application {
     private TextField urlField;
@@ -56,58 +56,132 @@ public class MirthJNLPLauncher extends Application {
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document doc = builder.parse(url.openStream());
 
+            List<String> coreJars = new ArrayList<>();
+            List<String> extensionJars = new ArrayList<>();
+            List<String> extensionJnlps = new ArrayList<>();
+
             NodeList jarList = doc.getElementsByTagName("jar");
-            List<String> jarFiles = new ArrayList<>();
             for (int i = 0; i < jarList.getLength(); i++) {
-                jarFiles.add(((Element) jarList.item(i)).getAttribute("href"));
+                Element jarElement = (Element) jarList.item(i);
+                String jarPath = jarElement.getAttribute("href");
+
+                if (jarPath.contains("extensions")) {
+                    extensionJars.add(jarPath);
+                } else {
+                    coreJars.add(jarPath);
+                }
             }
 
-            Element appDesc = (Element) doc.getElementsByTagName("application-desc").item(0);
-            String mainClass = appDesc.getAttribute("main-class");
+            // Detect and log extension JNLPs
+            NodeList extensionList = doc.getElementsByTagName("extension");
+            for (int i = 0; i < extensionList.getLength(); i++) {
+                Element extElement = (Element) extensionList.item(i);
+                String extJnlpPath = extElement.getAttribute("href");
+                extensionJnlps.add(extJnlpPath);
+                log("Detected extension JNLP: " + extJnlpPath);
+            }
 
-            log("Main Class: " + mainClass);
-            log("JARs: " + jarFiles);
+            log("Core JARs: " + coreJars);
+            log("Extension JARs: " + extensionJars);
+            log("Detected Extension JNLPs: " + extensionJnlps);
 
-            downloadAndLaunch(jnlpUrl, mainClass, jarFiles);
+            // Process extension JNLP files BEFORE downloading JARs
+            for (String extJnlp : extensionJnlps) {
+                String extJnlpUrl = jnlpUrl.substring(0, jnlpUrl.lastIndexOf("/")) + "/" + extJnlp;
+                log("Processing extension JNLP: " + extJnlpUrl);
+                parseExtensionJnlp(extJnlpUrl, extensionJars);
+            }
+
+            downloadAndLaunch(jnlpUrl, coreJars, extensionJars);
         } catch (Exception e) {
             log("Error: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
-    private void downloadAndLaunch(String jnlpUrl, String mainClass, List<String> jarFiles) {
+    private void parseExtensionJnlp(String extJnlpUrl, List<String> extensionJars) {
+        log("Fetching Extension JNLP: " + extJnlpUrl);
         try {
-            String baseUrl = jnlpUrl.substring(0, jnlpUrl.lastIndexOf("/"));
-            String cacheDir = "mirth-cache";
-            new File(cacheDir).mkdirs();
+            URL url = new URL(extJnlpUrl);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(url.openStream());
 
-            List<File> localJars = new ArrayList<>();
-            for (String jar : jarFiles) {
-                File localFile = new File(cacheDir, new File(jar).getName());
-                if (!localFile.exists()) {
-                    log("Downloading: " + jar);
-                    try (InputStream in = new URL(baseUrl + "/" + jar).openStream();
-                         FileOutputStream out = new FileOutputStream(localFile)) {
-                        byte[] buffer = new byte[4096];
-                        int bytesRead;
-                        while ((bytesRead = in.read(buffer)) != -1) {
-                            out.write(buffer, 0, bytesRead);
-                        }
-                    }
-                    log("Saved: " + localFile.getAbsolutePath());
-                }
-                localJars.add(localFile);
+            NodeList resourceList = doc.getElementsByTagName("resources");
+            if (resourceList.getLength() == 0) {
+                log("WARNING: No <resources> tag found in extension JNLP: " + extJnlpUrl);
+                return;
             }
 
-            launchMirth(mainClass, localJars);
+            for (int i = 0; i < resourceList.getLength(); i++) {
+                Element resourceElement = (Element) resourceList.item(i);
+                NodeList jarList = resourceElement.getElementsByTagName("jar");
+
+                if (jarList.getLength() == 0) {
+                    log("WARNING: No JARs found in <resources> of extension JNLP: " + extJnlpUrl);
+                }
+
+                for (int j = 0; j < jarList.getLength(); j++) {
+                    Element jarElement = (Element) jarList.item(j);
+                    String jarPath = jarElement.getAttribute("href");
+
+                    log("Found JAR in Extension JNLP: " + jarPath);
+                    extensionJars.add(jarPath);
+                }
+            }
+
+            log("Total JARs from " + extJnlpUrl + ": " + extensionJars);
         } catch (Exception e) {
-            log("Error: " + e.getMessage());
-            e.printStackTrace();
+            log("Error fetching extension JNLP: " + e.getMessage());
+        }
+    }
+
+    private void downloadAndLaunch(String jnlpUrl, List<String> coreJars, List<String> extensionJars) {
+        log("Starting download process...");
+
+        String baseUrl = jnlpUrl.substring(0, jnlpUrl.lastIndexOf("/"));
+        String cacheCoreDir = "mirth-cache/core";
+        String cacheExtensionsDir = "mirth-cache/extensions";
+
+        new File(cacheCoreDir).mkdirs();
+        new File(cacheExtensionsDir).mkdirs();
+
+        List<File> localJars = new ArrayList<>();
+
+        // Download Core JARs
+        for (String jar : coreJars) {
+            String jarUrl = baseUrl + "/" + jar;
+            File localFile = new File(cacheCoreDir, new File(jar).getName());
+
+            log("Downloading Core JAR: " + jarUrl);
+            if (!downloadFile(jarUrl, localFile)) {
+                log("WARNING: Missing core JAR: " + jarUrl);
+            }
+            localJars.add(localFile);
+        }
+
+        // Download Extension JARs
+        for (String jar : extensionJars) {
+            String jarUrl = baseUrl + "/" + jar;
+            File localFile = new File(cacheExtensionsDir, new File(jar).getName());
+
+            log("Downloading Extension JAR: " + jarUrl);
+            if (!downloadFile(jarUrl, localFile)) {
+                log("WARNING: Missing extension JAR: " + jarUrl);
+            }
+            localJars.add(localFile);
+        }
+
+        try {
+            launchMirth("com.mirth.connect.client.ui.Mirth", localJars);
+        } catch (Exception e) {
+            log("ERROR: Failed to launch Mirth - " + e.getMessage());
         }
     }
 
     private void launchMirth(String mainClass, List<File> jarFiles) throws Exception {
         log("Launching Mirth Client...");
+
         URL[] urls = jarFiles.stream().map(file -> {
             try {
                 return file.toURI().toURL();
@@ -127,5 +201,29 @@ public class MirthJNLPLauncher extends Application {
 
     private void log(String message) {
         logArea.appendText(message + "\n");
+    }
+
+    private boolean downloadFile(String urlStr, File destination) {
+        try {
+            log("Attempting to download: " + urlStr);
+            URL url = new URL(urlStr);
+
+            try (InputStream in = url.openStream(); FileOutputStream out = new FileOutputStream(destination)) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, bytesRead);
+                }
+            }
+
+            log("Saved: " + destination.getAbsolutePath());
+            return true;
+        } catch (FileNotFoundException e) {
+            log("WARNING: File not found: " + urlStr + " - Skipping.");
+            return false;
+        } catch (IOException e) {
+            log("ERROR: Failed to download " + urlStr + " - " + e.getMessage());
+            return false;
+        }
     }
 }
